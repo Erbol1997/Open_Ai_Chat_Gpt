78faea70f4a5aa4142d4b5ab2d80e5fbd0444439
Erbol <erbolclassic1297@gmail.com>
2023-02-26 22:41:24 +0500

    version 1.1

--- client/main.js
+++ client/main.js
@@ -1,10 +1,17 @@
-import bot from './public/bot.svg';
 import user from './public/user.svg';
+import bot from './public/bot.svg';
 
 const chatItems = document.getElementById('chat-items');
 const submitBtn = document.getElementById('submit');
 const textarea = document.querySelector('textarea');
 const form = document.querySelector('form');
+
+const lastQuestionContainer = document.getElementById('last-questions-items');
+const lastQuestionsItemsClearBtn = document.getElementById('last-questions-items-clear');
+
+const openSidebarBtn = document.getElementById('open');
+const closeSidebarBtn = document.getElementById('close');
+const sidebar = document.getElementById('sidebar');
 
 function generateUniqueId() {
   const timestamp = Date.now();
@@ -16,21 +23,31 @@ function generateUniqueId() {
 
 function userMessageRender(value) {
   return `<div class="user-message">
-            <img src="${user}" alt="user" />
-            <div class="user-question">
+            <img src=${user} alt="user" />
+            <pre class="user-question">
               ${value}
-            </div>
+            </pre>
           </div>
         `;
 }
 
 function botMessageRender(value, uniqueId) {
   return `<div class="bot-message">
-            <img src="${bot}" alt="bot" />
+            <img src=${bot} alt="bot" />
+            <i id="botAnswerCopy" class="fa-solid fa-clipboard"></i>
             <div class="bot-answer" id=${uniqueId}>
               ${value}
             </div>
           </div>
+        `;
+}
+
+function lasQuestionRender(value) {
+  return `<li class="long-text-hidden">
+            <i class="fa-regular fa-comment"></i>
+            ${value}
+            <i id="copyBtn" class="fa-solid fa-clipboard"></i>
+          </li>
         `;
 }
 
@@ -72,6 +89,12 @@ submitBtn.addEventListener('click', async (e) => {
 
   form.reset();
 
+  textarea.style.height = '55px';
+
+  lastQuestionContainer.innerHTML += lasQuestionRender(data.get('prompt'));
+
+  lastQuestionCopy();
+
   const uniqueId = generateUniqueId();
 
   chatItems.innerHTML += botMessageRender('', uniqueId);
@@ -81,6 +104,8 @@ submitBtn.addEventListener('click', async (e) => {
   const botMessageContainer = document.getElementById(uniqueId);
 
   loader(botMessageContainer);
+
+  botAnswerCopy();
 
   const response = await fetch('https://chat-gpt-8pmm.onrender.com', {
     method: 'POST',
@@ -115,3 +140,66 @@ form.addEventListener('keypress', function enterKey(event) {
     submit.click();
   }
 });
+
+lastQuestionsItemsClearBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  lastQuestionContainer.innerHTML = '';
+});
+
+openSidebarBtn.addEventListener('click', (e) => {
+  openSidebarBtn.style.display = 'none';
+  closeSidebarBtn.style.display = 'inline-block';
+  sidebar.style.display = 'flex';
+});
+
+closeSidebarBtn.addEventListener('click', (e) => {
+  openSidebarBtn.style.display = 'inline-block';
+  closeSidebarBtn.style.display = 'none';
+  sidebar.style.display = 'none';
+});
+
+textarea.addEventListener('input', (e) => {
+  textarea.style.height = '55px';
+  let scHeight = e.target.scrollHeight;
+  textarea.style.height = `${scHeight}px`;
+});
+
+function lastQuestionCopy() {
+  const copyBtns = document.querySelectorAll('#copyBtn');
+  for (const copyBtn of copyBtns) {
+    copyBtn.addEventListener('click', (e) => {
+      let itemData = copyBtn.parentElement;
+      var inp = document.createElement('input');
+      inp.value = itemData.innerText;
+      document.body.appendChild(inp);
+      inp.select();
+      if (document.execCommand('copy')) {
+        console.log('success');
+      } else {
+        console.log('error');
+      }
+
+      document.body.removeChild(inp);
+    });
+  }
+}
+
+function botAnswerCopy() {
+  const botAnswerCopyBtns = document.querySelectorAll('#botAnswerCopy');
+  for (const botAnswerCopyBtn of botAnswerCopyBtns) {
+    botAnswerCopyBtn.addEventListener('click', (e) => {
+      let itemData = botAnswerCopyBtn.parentElement.children[2];
+      var inp = document.createElement('input');
+      inp.value = itemData.innerText;
+      document.body.appendChild(inp);
+      inp.select();
+      if (document.execCommand('copy')) {
+        console.log('success');
+      } else {
+        console.log('error');
+      }
+
+      document.body.removeChild(inp);
+    });
+  }
+}
